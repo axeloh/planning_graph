4cd1ce3a60dfdec3b33b64ad3103a50be97cbd1e
axeloh <axel.harstad@gmail.com>
2018-11-25 21:46:31 +0100

    Started building the structure of the problem

---
+++ graph_plan.py
@@ -0,0 +1,27 @@
+
+# Propositions
+brushClean = "BrushClean"
+rollerClean = "RollerClean"
+mouldingsPainted = "MouldingsPainted"
+wallPainted = "WallPainted"
+picturesHanging = "PicturesHanging"
+
+
+# Initial conditions
+iCond = [brushClean, rollerClean, picturesHanging]
+
+# Goals
+goals = [mouldingsPainted, wallPainted, picturesHanging]
+
+# Actions
+def brushPaintMouldings(conditions):
+    if 
+
+def graph_plan(initial_state, goals, action):
+        """
+        :param initial_state: 
+        :param goals: 
+        :param action: 
+        :return: 
+        """
+
